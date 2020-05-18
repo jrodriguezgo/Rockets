5559fef09cf4a5c58f6e41a4ff2b37180a8d8fd5
jrodriguezgo <jaime.stone@hotmail.com>
2020-05-18 14:20:03 +0200

    Codi de Resultats

--- Rockets/src/capaApplication/Main.java
+++ Rockets/src/capaApplication/Main.java
@@ -12,45 +12,50 @@ public class Main {
 	public static void startGame(int time, int distance) {
 		boolean out = false;
 		int star_time = 0;
-
-		Coet c = new Coet("Souvlaki");
-		
+		Coet c = new Coet("Souvlaki");	
 		System.out.printf("Rocket: "+ c.getName() + "\n");
 		System.out.print("Starting competition. Circuit length: " + distance +". Max time: " + time + "\n");
-		
-		while(!out && star_time < time) {
-			
+		while(!out && star_time < time && c.getFuel() > 0) {
 			System.out.print("Please write the acceleration\n");
 			int acelerar = Keyboard.readInt();
 			int set;
-			
+			int current_distance = 0;
 			set = c.calculateSpeedDistance(acelerar, 2);
-			set = (int) c.calculateFuel();
-			
+			set = (int) c.calculateFuel();		
 			if(set == 0) {
 				star_time += 2;
 				
 			}else {
 				star_time += set;
 			}
-
 			System.out.print("Current time: " + star_time + " Acceleration: "  + c.getAcceleration() 
-			+ " Speed: " + c.getSpeed() + " Distance: " + c.getDistance() + " Circuit: " + distance 
-			);
-			out = Fuel(distance, c);
+			+ " Speed: " + c.getSpeed() + " Distance: " + c.getDistance() + " Circuit: " + distance);
+			
+			current_distance += c.getDistance();
+			out = Fuel(distance, c, current_distance);
 			
 		}
-		
+		Score(out, star_time, c);		
 	}
 	
-	private static boolean Fuel(int distance, Coet coet) {	
+	private static boolean Fuel(int distance, Coet coet, int current_distance) {	
 		if (coet.getFuel() <= 0) {
 			System.out.print(" Fuel: 0\n");
 		} else {
-			System.out.print(" Fuel: " + coet.getFuel()+"\n");
+			System.out.print(" Fuel: " + coet.getFuel() +"\n");
+			
+		}
+		if(current_distance >= distance && coet.getFuel() >= 0) {
+			return true;
 		}
 		return false;
 	}
 	
-		
+	public static void Score(boolean end, int time, Coet coet) {
+		if(end) {
+			System.out.print("And the winner is: " + coet.getName() + " with a time of " +time);
+		}else {
+			System.out.print("There is no winner, sorry!");
+		}
+	}	
 }
